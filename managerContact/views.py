ba94960d5f7a125e835811a775817bd0bc2a1ef4
yandaki <boayves@gmail.com>
2023-07-28 23:14:25 +0000

    Add features add new phone numbers to a contact

--- managerContact/views.py
+++ managerContact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , HttpResponseRedirect
+from django.shortcuts import render , HttpResponseRedirect , get_object_or_404
 from django.views import generic
 from .models import Person, NumberTel
 from django.utils import timezone
@@ -13,6 +13,17 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Person.objects.all().order_by("-date_creation")
 
+
+def addNumberView(request, person_id):
+    # Get item person by id
+    person = Person.objects.get(pk=person_id)
+    return render(request , "managerContact/addNumber.html", {"person":person})
+    
+
+def updateContactView(request , person_id):
+    # get item person which correspond to id
+    person = Person.objects.get(id=person_id)
+    return render(request , "managerContact/updateContact.html" , {"person":person})
 
 def addPerson(request):
     # get property name , email, and number
@@ -36,3 +47,15 @@ def addPerson(request):
         return render(request , "managerContact/index.html" , {"message_error":"Contacts already present"})
     
     return HttpResponseRedirect(reverse("managerContact:index" , args=()))
+
+
+def addOtherNumber(request , person_id):
+    # get the element who correspond to id
+    person = get_object_or_404(Person , pk = person_id)
+    
+    new_number = request.POST.get('new_number_tel')
+    
+    # add number to this person
+    person.numbertel_set.create(other_number=new_number)
+    # redirection to index
+    return HttpResponseRedirect(reverse('managerContact:index' , args=()))
