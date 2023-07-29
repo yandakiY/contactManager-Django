f8cb846215c630614ec7a9d074952eef79b9be48
yandaki <boayves@gmail.com>
2023-07-29 00:39:59 +0000

    Implementation Update a contact

--- managerContact/urls.py
+++ managerContact/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 app_name = "managerContact"
 urlpatterns = [
     path('' , views.IndexView.as_view() , name="index"),
-    path('addPerson' , views.addPerson , name="addPerson")
+    path('addPerson' , views.addPerson , name="addPerson"),
+    path('addNumber/<int:person_id>' , views.addNumberView , name="addNumber"),
+    path('addOtherNumber/<int:person_id>' , views.addOtherNumber , name="addOtherNumber"),
+    path('updateContact/<int:person_id>' , views.updateContactView , name="updateContact"),
+    path('updateContactForm<int:person_id>', views.updateContact , name="updateContactForm" )
 ]
--- managerContact/views.py
+++ managerContact/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from .models import Person, NumberTel
 from django.utils import timezone
 from django.urls import reverse
+from django.utils.html import escape
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -59,3 +60,39 @@ def addOtherNumber(request , person_id):
     person.numbertel_set.create(other_number=new_number)
     # redirection to index
     return HttpResponseRedirect(reverse('managerContact:index' , args=()))
+
+def updateContact(request , person_id):
+    # get the item which correspond 
+    # change name , email and numbers 
+    person = get_object_or_404(Person , id=person_id)
+    
+    person.name_person = escape(request.POST.get('name_person'))
+    
+    person.email = escape(request.POST.get('email'))
+    person.save()
+    
+    # get all numbers associate to this person
+    # browse each number 
+    # update and save
+    
+    # get all numbers from form
+    for i , number in enumerate(person.numbertel_set.all() , start=1):
+        # input from formulaire in htmk file
+        input = request.POST.get(f'number{i}')
+        # 
+        # person.numbertel_set.get(other_number=number).other_number = input 
+        # delete the number present
+        person.numbertel_set.get(other_number=number).delete()
+        # and create a new number with the value which come to the form in html file
+        person.numbertel_set.create(other_number=input)
+        # person.save()
+        
+        # print(f'Depuis Forms: {input} Database {number} , index:{i}')
+    
+    
+    # Save , first change
+    # person.save()
+    
+    # redirection to index
+    return HttpResponseRedirect(reverse("managerContact:index" , args=()))
+    
